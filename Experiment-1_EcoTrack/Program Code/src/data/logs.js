78b71b02fd09e46b1cc7c5ff13f6e2919fb4adeb
ds-0907 <23bai70166@cuchd.in>
2026-01-28 10:13:59 +0530

    Delete Experiment-1_EcoTrack directory

--- Experiment-1_EcoTrack/Program Code/src/data/logs.js
+++
@@ -1,7 +0,0 @@
-const logs = [
-  { id: 1, activity: "Car Travel", carbon: 28 },
-  { id: 2, activity: "Electricity Usage", carbon: 12 },
-  { id: 3, activity: "Cycling", carbon: 0 },
-];
-
-export default logs;
