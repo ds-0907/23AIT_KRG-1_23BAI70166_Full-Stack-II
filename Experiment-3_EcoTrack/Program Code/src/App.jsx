d8d4b7873026a2c0fd09c819b6adc4e136f3d97a
ds-0907 <23bai70166.cuchd.in>
2026-02-01 20:14:31 +0530

    Experiment-3

---
+++ Experiment-3_EcoTrack/Program Code/src/App.jsx
@@ -0,0 +1,27 @@
+import DashboardAnalytics from "./pages/DashboardAnalytics";
+import Logs from "./pages/Logs";
+import DashboardLayout from "./pages/DashboardLayout";
+import DashboardSummary from "./pages/DashboardSummary";
+import ProtectedRoute from "./routes/ProtectedRoute.jsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/login";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Header title="EcoTrack" />
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<ProtectedRoute><DashboardLayout /></ProtectedRoute>}>
+          <Route index element={<DashboardSummary />} />
+          <Route path="summary" element={<DashboardSummary />} />
+          <Route path="analytics" element={<DashboardAnalytics />} />
+          <Route path="logs" element={<Logs />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
